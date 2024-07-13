19c62251501eead51a618e4e86dec0a023042192
Ajay Gupta <ajay@instaastro.in>
2024-07-13 14:33:32 +0530

    Updates

---
+++ precache-manifest.cfd135ac1d5c4d236c490159216e42ac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f2935e048e60b14f5813cf44aedf03",
+    "url": "/Canteen-Management/index.html"
+  },
+  {
+    "revision": "75d1575a64b91ee36602",
+    "url": "/Canteen-Management/static/css/main.ff51c147.chunk.css"
+  },
+  {
+    "revision": "775f5db66aeda0ccf0b3",
+    "url": "/Canteen-Management/static/js/2.e54e6696.chunk.js"
+  },
+  {
+    "revision": "70c068b2fb9d959ad245",
+    "url": "/Canteen-Management/static/js/3.730e5367.chunk.js"
+  },
+  {
+    "revision": "75d1575a64b91ee36602",
+    "url": "/Canteen-Management/static/js/main.c1903710.chunk.js"
+  },
+  {
+    "revision": "f24145e1cd1bf06fbaae",
+    "url": "/Canteen-Management/static/js/runtime~main.e30128b1.js"
+  },
+  {
+    "revision": "abae6e191ca5241e15fe437841800973",
+    "url": "/Canteen-Management/static/media/burger.abae6e19.png"
+  },
+  {
+    "revision": "5edac453059b3e307a335ae5ee0eb5bc",
+    "url": "/Canteen-Management/static/media/chef.5edac453.png"
+  },
+  {
+    "revision": "d99c38651593211dabb097652e86a0a9",
+    "url": "/Canteen-Management/static/media/chefDual.d99c3865.png"
+  },
+  {
+    "revision": "3cc2b1a34fe2eede67240973e1be7b12",
+    "url": "/Canteen-Management/static/media/combo.3cc2b1a3.png"
+  },
+  {
+    "revision": "df09abc1de436c922e2a684620c9bd3d",
+    "url": "/Canteen-Management/static/media/noodles.df09abc1.png"
+  },
+  {
+    "revision": "802fd734996f8e13002b3b43eec3f072",
+    "url": "/Canteen-Management/static/media/pasta.802fd734.png"
+  }
+]);
